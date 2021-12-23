c6a90da42480a338b624ad5e6108a26807fefef9
hüseyin çakmak <huseyincakmak@huseyins-MacBook-Air.local>
2021-12-24 00:35:01 +0300

    Add a Procfile

--- api/index.js
+++ api/index.js
@@ -21,6 +21,6 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 
-app.listen(process.env.PORT || 5001, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
 });
